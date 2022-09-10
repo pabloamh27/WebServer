8f37ba844660919fb581cbad79ff4747565f9097
Royner39 <roynerarturo39@gmail.com>
2022-09-10 14:14:45 -0600

    Web Server básico

---
+++ web_server/target/debug/deps/web_server-3a446d587cfad9da.d
@@ -0,0 +1,5 @@
+/home/royner39/Escritorio/web_server/target/debug/deps/web_server-3a446d587cfad9da.rmeta: src/main.rs
+
+/home/royner39/Escritorio/web_server/target/debug/deps/web_server-3a446d587cfad9da.d: src/main.rs
+
+src/main.rs:
---
+++ web_server/target/debug/web_server.d
@@ -0,0 +1,1 @@
+/home/royner39/Escritorio/web_server/target/debug/web_server: /home/royner39/Escritorio/web_server/src/main.rs
